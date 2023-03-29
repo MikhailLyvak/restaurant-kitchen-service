0d1a4ba7d84f0dfd7fb3f938c5f296260052bfbf
IZevsl <michael.lyvak@gmail.com>
2023-03-29 11:16:49 +0300

    Created models and migrations

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+from django.db.models import UniqueConstraint
+from django.conf import settings
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return {self.name}
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(max_length=255)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+        ordering = ["-years_of_experience"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("kitchen:cook-detail", kwargs={"pk": self.pk})
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="cooks")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
