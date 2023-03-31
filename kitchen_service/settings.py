ac225cc836ab40a29c24280a9d1c138b578dee98
IZevsl <michael.lyvak@gmail.com>
2023-03-31 14:50:24 +0300

    Fixed requirements.txt filename...

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",
-    "django-insecure-l9ir!z)hz=3_=@+x*zi+&1y7ui^hj$z@ksp$ipm6$@ti8p9yv-"
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
